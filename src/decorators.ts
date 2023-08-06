d62530aaf775bd780a3a3e40edf3d241b9291527
Abutalib Namazov <abutalib@mit.edu>
2023-08-06 01:46:23 -0500

    clean a bit

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,8 +2,20 @@ import { Request, RequestHandler, Response } from "express";
 
 export function makeRoute(f: Function): RequestHandler {
   return (req: Request, res: Response) => {
-    const paramNames = f.toString().match(/\((.*?)\)/)![1].split(",").map((param: string) => param.split("=")[0].trim());
-    const args = paramNames.map(name => name !== "session" ? req.params[name] || req.query[name] || req.body[name] : req.session);
+    const reqMap = (name: string) => {
+      if (name === "session" || name == "query") {
+        return req[name];
+      }
+      const ret = req.params[name] || req.query[name] || req.body[name];
+      if (ret === undefined || ret === null) {
+        console.log(`${ret} does not exist in the query`);
+      }
+      return ret;
+    }
+
+    const args = f.toString().match(/\((.*?)\)/)![1].split(",") // Simple regex to get "name: type" items in signature
+                  .map((param: string) => param.split("=")[0].trim()) // remove default values and whitespaces
+                  .map(reqMap);
     const result = f.apply(null, args);
     if (result instanceof Promise) {
       result.then(result => res.json(result)).catch(err => {
